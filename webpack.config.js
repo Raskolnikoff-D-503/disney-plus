f0690aaa6bdb37b3ca772567a08a31222676049b
Raskolnikoff-D-503 <gaagevgenii@gmail.com>
2022-04-06 00:10:14 +0300

    project configuration

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const getFileLoaderOptions = () => ({
+  name: `[name].[ext]`,
+  path: path.resolve(__dirname, 'public'),
+});
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {path: path.resolve(__dirname, 'public'), filename: 'bundle.js'},
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    open: false,
+    port: 3000,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true,
+          },
+        },
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+        },
+      },
+      {
+        test: /\.(s*)css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(webp|png|jpe?g|svg|gif)$/,
+        use: {
+          loader: 'file-loader',
+          options: getFileLoaderOptions(),
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({template: './src/index.html'}),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+  ],
+};

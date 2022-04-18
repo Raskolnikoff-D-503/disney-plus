3c0de474cf9994201c94160815039adba3c5a787
Raskolnikoff-D-503 <gaagevgenii@gmail.com>
2022-04-18 22:52:27 +0300

    setting firebase

---
+++ src/firebase.ts
@@ -0,0 +1,25 @@
+import {initializeApp} from 'firebase/app';
+import {getFirestore} from 'firebase/firestore';
+import {getAuth, GoogleAuthProvider} from 'firebase/auth';
+import {getStorage} from 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBNtUOnUUqgrnzJXUMX3k9Fv6HCqY26BGk',
+  authDomain: 'disney-plus-app-e2845.firebaseapp.com',
+  projectId: 'disney-plus-app-e2845',
+  storageBucket: 'disney-plus-app-e2845.appspot.com',
+  messagingSenderId: '504420337204',
+  appId: '1:504420337204:web:1195bfef74d78657d66c61',
+  measurementId: 'G-CL81CT54VE',
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const db = getFirestore(firebaseApp);
+const auth = getAuth(firebaseApp);
+const provider = new GoogleAuthProvider();
+const storage = getStorage(firebaseApp);
+
+export {auth, provider, storage};
+export default db;
